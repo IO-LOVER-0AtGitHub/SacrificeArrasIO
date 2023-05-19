e7de33639cf93540009ce47393474a37622fc55b
Glitch (arras-mayhem) <none>
2023-05-19 08:45:21 +0000

    🚣🍃 Checkpoint ./bundle.js:15958994/16

--- bundle.js
+++ bundle.js
@@ -4744,7 +4744,7 @@
                     visible: 0,
             id: "Fly-Event",
             type: "main",
-            code: "flyio-worldwide-x",
+            code: "flyio-montreale-x",
             at: p.flyio("arras"),
              featured: 1, // For Featured Status.
 
